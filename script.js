227d34e8bc07267f079de1a7e143e6fe2c9436af
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-05 06:28:05 -0500

    adding js code for timer and date

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
-var nineAm = $("#text9am");
-var span9am = $("#9am");
-var tenAm = $("#text10am");
-var hour = moment().format('h a');
-var hourSched = $(span9am).text();
 
-// var userInput = ""
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
-$("#currentDay").text(currentDate + " " + currentHour);
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
 
 function initPage() {
   var init = JSON.parse(localStorage.getItem("9 am"));
